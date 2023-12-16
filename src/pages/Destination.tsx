2ee34d29294d66652df65a23986c38b2c8ea3dd9
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-12-17 05:36:34 +0800

    update pages

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import BGMobile from "../assets/destination/background-destination-mobile.jpg";
 import BGTablet from "../assets/destination/background-destination-tablet.jpg";
 import BGDesktop from "../assets/destination/background-destination-desktop.jpg";
+import Moon from "../assets/destination/image-moon.png";
 
 const DestinationContainer = styled.section`
   width: 100%;
@@ -9,6 +10,11 @@ const DestinationContainer = styled.section`
   background-image: url(${BGMobile});
   background-repeat: no-repeat;
   background-size: cover;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   @media (min-width: 600px) {
     background-image: url(${BGTablet});
   }
@@ -16,8 +22,60 @@ const DestinationContainer = styled.section`
     background-image: url(${BGDesktop});
   }
 `;
+const StyledH2 = styled.h2`
+  text-transform: uppercase;
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+  letter-spacing: 5px;
+  & > span {
+    color: gray;
+    font-weight: bold;
+  }
+`;
+const StyledH3 = styled.h3`
+  text-transform: uppercase;
+  font-family: "Bellefair", sans-serif;
+  font-size: 3rem;
+  font-weight: normal;
+  letter-spacing: 5px;
+`;
+const StyledList = styled.ul`
+  width: 60%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+const StyledListItem = styled.li`
+  list-style: none;
+  text-transform: uppercase;
+  font-family: "Barlow Condensed", sans-serif;
+`;
+const StyledImg = styled.img`
+  transform: scale(0.63);
+`;
+const StyledP = styled.p``;
 const Destination = () => {
-  return <DestinationContainer></DestinationContainer>;
+  return (
+    <DestinationContainer>
+      <StyledH2>
+        <span>01</span> Pick your destination
+      </StyledH2>
+      <StyledImg src={Moon} alt="moon" />
+      <StyledList>
+        <StyledListItem>Moon</StyledListItem>
+        <StyledListItem>Mars</StyledListItem>
+        <StyledListItem>Europa</StyledListItem>
+        <StyledListItem>Titan</StyledListItem>
+      </StyledList>
+      <StyledH3>Moon</StyledH3>
+      <StyledP>
+        See our planet as you’ve never seen it before. A perfect relaxing trip
+        away to help regain perspective and come back refreshed. While you’re
+        there, take in some history by visiting the Luna 2 and Apollo 11 landing
+        sites.
+      </StyledP>
+    </DestinationContainer>
+  );
 };
 
 export default Destination;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,93 @@ const HomeContainer = styled.section`
   background-image: url(${BGMobile});
   background-repeat: no-repeat;
   background-size: cover;
+  text-align: center;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   @media (min-width: 600px) {
     background-image: url(${BGTablet});
+  }
+  @media (min-width: 700px) {
+    text-align: left;
+    padding: 35% 4rem 4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 5rem 5rem 1fr;
+    column-gap: 2rem;
   }
   @media (min-width: 1024px) {
     background-image: url(${BGDesktop});
   }
 `;
+const StyledH1 = styled.h1`
+  text-transform: uppercase;
+  font-family: "Bellefair", serif;
+  font-size: 4.5rem;
+`;
+const StyledH2 = styled.h2`
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  @media (min-width: 700px) {
+    font-size: 1.7rem;
+    line-height: 1.5;
+  }
+`;
+const StyledP = styled.p`
+  line-height: 1.5;
+  @media (min-width: 700px) {
+    line-height: 2;
+  }
+`;
+const Circle = styled.div`
+  background-color: white;
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+  & > span {
+    color: black;
+    text-transform: uppercase;
+    font-family: "Bellefair", serif;
+    font-size: 2rem;
+  }
+  @media (min-width: 700px) {
+    margin-top: 0;
+    grid-column: 2 / 3;
+    grid-row: 1 / 4;
+    justify-self: center;
+    align-self: end;
+    & > span {
+      font-size: 1.8rem;
+    }
+  }
+`;
+// tablet and desktop styles not done yet
+
 const Home = () => {
-  return <HomeContainer></HomeContainer>;
+  return (
+    <HomeContainer>
+      <StyledH2>So, you want to travel to</StyledH2>
+      <StyledH1>Space</StyledH1>
+      <StyledP>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </StyledP>
+      <Circle>
+        <span>Explore</span>
+      </Circle>
+    </HomeContainer>
+  );
 };
 
 export default Home;
