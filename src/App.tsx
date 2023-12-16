fc2373707de61d26b55f7127e21ae9cdc4944456
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-12-17 01:22:16 +0800

    set up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,52 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import Home from "./pages/Home";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+import Destination from "./pages/Destination";
+import MainLayout from "./MainLayout";
+import styled from "styled-components";
+
 import "./App.css";
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: var(--app-height);
+`;
+
 const App = () => {
-  return <></>;
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const setAppHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", setAppHeight);
+    setAppHeight();
+    return () => window.removeEventListener("resize", setAppHeight);
+  }, []);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout menu={menu} setMenu={setMenu} />}>
+        <Route index element={<Home />} />
+        <Route path="destination" element={<Destination />} />
+        <Route path="crew" element={<Crew />} />
+        <Route path="technology" element={<Technology />} />
+      </Route>
+    )
+  );
+  return (
+    <AppContainer>
+      <RouterProvider router={router} />
+    </AppContainer>
+  );
 };
 export default App;
