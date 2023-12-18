054fb61ff1c0ba2caa43a2c43f928e31762e9923
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-12-18 16:19:10 +0800

    create shared styles

---
+++ src/assets/styles/SharedStyles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const SharedSection = styled.section`
+  width: 100%;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
