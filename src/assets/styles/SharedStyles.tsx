e217b8f5374c474dc5e2b6c80352906ba4561e21
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-12-20 22:19:34 +0800

    update shared styles

--- src/assets/styles/SharedStyles.tsx
+++ src/assets/styles/SharedStyles.tsx
@@ -1,13 +1,56 @@
+/* eslint-disable react-refresh/only-export-components */
 import styled from "styled-components";
+import { LabelProps } from "../../interfaces";
 
-export const SharedSection = styled.section`
+export const Section = styled.section`
   width: 100%;
-  height: 100vh;
+  height: auto;
+  min-height: 100vh;
+  padding: 6.5rem 2rem 4rem;
+  gap: 2rem;
   background-repeat: no-repeat;
   background-size: cover;
-  padding: 2rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+export const H2 = styled.h2`
+  text-transform: uppercase;
+  font-family: "Barlow Condensed", sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+  letter-spacing: 5px;
+  & > span {
+    color: gray;
+    font-weight: bold;
+  }
+  @media (min-width: 560px) {
+    margin-top: 1rem;
+    font-size: 1.1rem;
+  }
+  @media (min-width: 720px) {
+    font-size: 1.5rem;
+    margin-top: 4.5rem;
+    align-self: flex-start;
+  }
+`;
+export const H3 = styled.h3`
+  text-transform: uppercase;
+  font-family: "Bellefair", sans-serif;
+  font-weight: normal;
+  letter-spacing: 5px;
+`;
+export const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+export const RadioBtn = styled.input`
+  display: none;
+`;
+export const Label = styled.label<LabelProps>`
+  display: grid;
+  place-items: center;
+  cursor: pointer;
 `;
